6e18407f65c77bdcf2f3c089111d4c36b01ae9d5
Abdulrehmanlodhi7 <144684224+Abdulrehmanlodhi7@users.noreply.github.com>
2023-09-12 15:56:31 +0500

    Add files via upload

---
+++ Calculator.js
@@ -0,0 +1,38 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    {
+        type: "number",
+        name: "num1",
+        message: "Enter your first number:"
+    },
+    {
+        type: "number",
+        name: "num2",
+        message: "Enter your second number:"
+    },
+    {
+        type: "list",
+        name: "operator",
+        choices: ["Add", "Subtract", "Multiply", "Divide"],
+        message: "Select the operator you want to perform:"
+    },
+]);
+let result;
+switch (answer.operator) {
+    case 'Add':
+        result = answer.num1 + answer.num2;
+        console.log("The answer for addition is:" + result);
+        break;
+    case 'Subtract':
+        result = answer.num1 - answer.num2;
+        console.log("The answer for subtraction is:" + result);
+        break;
+    case 'Multiply':
+        result = answer.num1 * answer.num2;
+        console.log("The answer for multiplication is:" + result);
+        break;
+    case 'Divide':
+        result = answer.num1 / answer.num2;
+        console.log("The answer for division is:" + result);
+        break;
+}
